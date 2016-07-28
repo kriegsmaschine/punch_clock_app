a1a47de6f60d917b8261c14a0e67bbfcacef75c5
kriegsmaschine <scukras7@gmail.com>
2016-07-27 23:49:34 -0400

    first

--- kronos_clock/wsgi.py
+++ kronos_clock/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for kronos_clock project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
@@ -10,17 +8,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kronos_clock.settings")
 
-application = get_wsgi_application()
-
-try:
-	from dj_static import Cling
-	application = Cling(get_wsgi_application())
-except:
-	pass
-#application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
