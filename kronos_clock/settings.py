2e71fad9a8df8266d303fb173213b027248e7626
kriegsmaschine <scukras7@gmail.com>
2016-07-28 16:46:45 -0400

    debug = False

--- kronos_clock/settings.py
+++ kronos_clock/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '29dh!3@ot91d*!60)0x%%9)ndtb)cn#4_uj72=^(@f#h!(fpfc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
