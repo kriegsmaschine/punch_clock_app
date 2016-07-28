d38f7a9ca8c96acab62a3c32c7ea31f95c837f73
kriegsmaschine <scukras7@gmail.com>
2016-07-28 00:59:40 -0400

    update wsgi.py

--- kronos_clock/wsgi.py
+++ kronos_clock/wsgi.py
@@ -7,6 +7,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
 from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kronos_clock.settings")
+
+application = DjangoWhiteNoise(get_wsgi_application())
